31d92f71f9667827cb5a45a6c0367a50e4feae9b
saikumar <saikumar@digisprint.com>
2023-11-22 10:17:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Clock from 'react-clock';
+import 'react-clock/dist/Clock.css'
 
 function App() {
+  const [time , setTime] = useState(new Date());
+  useEffect(()=>{
+    setInterval(()=>{
+      setTime(new Date())
+    },1000)
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor:'pink',display:"inline-block",top:"50%",left:"50%",position:"absolute",
+    transform:"translate(-50%,-50%)",padding:"12vh"
+    }}>
+  
+    <Clock value={time}  size={300} renderNumbers={true}/>
     </div>
   );
 }
